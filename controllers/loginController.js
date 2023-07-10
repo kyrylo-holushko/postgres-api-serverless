99a1f6878b06629123ccce22fedf8f9d02a861ca
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-07-10 17:38:38 -0400

    JWT implemented

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,8 @@
 const Login = require('../models/loginModel');
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
+const { JWTSECRET, JWTEXPIRY } = process.env;
 
 exports.login = (req, res) => {
     const { usernameOrEmail, password } = req.boy;
@@ -26,7 +29,8 @@ exports.login = (req, res) => {
     }
     bcrypt.compare(password, data.password).then((result) => {
         if (result === true) {
-            // generate jwt.sign using data.id
+            const token = jwt.sign({ 'User ID': data.id }, JWTSECRET, { expiresIn: JWTEXPIRY });
+            res.json({ message: 'login successful', token: token });
         } else {
             res.status(500).json({ message: 'Could not log in. Passwords do not match!'});
         }
