7a7a6ad70cc351191b8d45811efd699678fd9ccc
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-07-10 15:10:37 -0400

    Error throwing for signup

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,24 +1,19 @@
 const SignUp = require('../models/signupModel');
 const bcrypt = require('bcrypt');
 
-/* encrypt = async (password) => {
-    const salted = await bcrypt.hash(data.password, 10);
-    console.log('Inside encrypt, after hashing', salted);
-    return salted;
-}; */
-
 exports.signup = (req, res) => {
     if(req.body.password!==req.body.passwordConfirmed)
         res.status(500).json({ message: "Passwords don't match, Account not created!"}) 
     else {
         let { passwordConfirmed, ...data } = req.body;
-        bcrypt.hash(data.password, 10).then(hash=>{data.password = hash;});
-        console.log('Encrypted Password is: ', data.password);
-        console.log('SignUp Controller');
-        const result = SignUp.post(data);
-        if(result instanceof Error)
-            res.status(500).json({ message: result });
-        else 
-            res.status(201).json({ message: 'New User Created', data: result});
+        bcrypt.hash(data.password, 10).then(hash=>{data.password = hash;}).catch(e=>{console.log('Encryption Failed.')});
+        SignUp.post(data).then((result)=>{
+            if(result instanceof Error)
+                throw result;
+            else
+                res.status(201).json({ message: 'New User Created', data: result});
+        }).catch(e=>{
+            res.status(500).json({ message: e.message });
+        });
     }
 };
--- models/signupModel.js
+++ models/signupModel.js
@@ -1,26 +1,14 @@
 const db = require('../pgConnect');
 
 exports.post = async (user) => {
-    console.log(user);
-    console.log('SignUp Model');
     try {
         const UserExists = await db.sql`SELECT * FROM users WHERE email = ${user.email}`;
         if(UserExists.length>0){
-            console.log('USEREXISTS: ', UserExists);
-            throw 'An account with this email already exists!';
+            return Error('An account with this email already exists!');
         }
         const result = await db.sql`INSERT INTO users (username, email, password) VALUES (${user.username}, ${user.email}, ${user.password}) RETURNING *`;
-        //sql`INSERT INTO users (username, password, email) VALUES (${user.username}, ${user.password}, ${user.email}) returning *`;
-        console.log('This is the result of INSERT: ', result[0]);
         return result[0];
     } catch(error) {
         console.log(error);
-        return error.message;
-        // return message, example: if user creates account with existing email
     }  
-
-    /* const [results, metadata] = await sequelize.query(`SELECT * FROM users`);
-    console.log('Here are the results', results);
-    console.log('Here is the metadata', metadata);
-    return results; */
 };
