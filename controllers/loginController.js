a8d41551281a6e2301e4229c92a0a9b47cea385f
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-07-11 12:32:42 -0400

    login controller refactor

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,6 +4,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { JWTSECRET, JWTEXPIRY } = process.env;
 
+function compareAndEncrypt(password, data, res) {
+    bcrypt.compare(password, data.password).then((result) => {
+        if (result === true) {
+            const token = jwt.sign({ 'User ID': data.id }, JWTSECRET, { expiresIn: JWTEXPIRY });
+            res.json({ message: 'login successful', token: token });
+        } else {
+            res.status(500).json({ message: 'Could not log in. Passwords do not match!'});
+        }
+    });  
+};
+
 exports.login = (req, res) => {
     const { usernameOrEmail, password } = req.body;
     let data;
@@ -12,7 +23,7 @@ exports.login = (req, res) => {
             if(result instanceof Error)
                 throw result;
             else
-                data = result;
+                compareAndEncrypt(password, result, res);
         }).catch(e=>{
             res.status(500).json({ message: e.message });
         });
@@ -22,17 +33,9 @@ exports.login = (req, res) => {
             if(result instanceof Error)
                 throw result;
             else
-                data = result;
+                compareAndEncrypt(password, result, res);
         }).catch(e=>{
             res.status(500).json({ message: e.message });
         });
-    }
-    bcrypt.compare(password, data.password).then((result) => {
-        if (result === true) {
-            const token = jwt.sign({ 'User ID': data.id }, JWTSECRET, { expiresIn: JWTEXPIRY });
-            res.json({ message: 'login successful', token: token });
-        } else {
-            res.status(500).json({ message: 'Could not log in. Passwords do not match!'});
-        }
-    });  
+    }  
 };
