cf53d96d5e3bc23434655e6ce702fa5cf4dfbc77
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-07-13 11:55:28 -0400

    get bags works

--- models/bagModels.js
+++ models/bagModels.js
@@ -2,8 +2,8 @@ const db = require('../pgConnect');
 
 exports.findAllBags = async (uid) => {
     try {
-        const hasAnyBags = await db.sql`SELECT bid, bname FROM users WHERE uid = ${uid}`;
-        if(!hasAnyBags)
+        const hasAnyBags = await db.sql`SELECT bid, bname FROM bags WHERE uid = ${uid}`;
+        if(hasAnyBags.length===0)
             return Error('You do not have any bags. Would you like to create a bag?');
         else
             return hasAnyBags;
