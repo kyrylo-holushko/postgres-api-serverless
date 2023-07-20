b1dcf6a638993259fea3f267e30d0a8ebeabbe2d
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-07-20 10:55:56 -0400

    get Items implemented

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,11 +1,25 @@
 const Item = require('../models/itemModel');
 
 exports.getItems = (req, res) => {
-
+    Item.findAllItems(req.userData.userID, req.body.bid).then((result)=>{
+    if(result instanceof Error)
+        throw result;
+    else
+        res.status(200).json({ data: result });
+    }).catch(e=>{
+        res.status(404).json({ message: e.message });
+    });
 };
 
 exports.createItem = (req, res) => {
+    let data = processItemOptionals(req.body);
 
+    /* let data = processBagOptionals(req.body);
+    Bag.createBag(data, req.userData.userID).then((result)=>{
+        res.status(201).json({ message: 'New Bag Created', data: result });
+    }).catch(e=>{
+        res.status(500).json({ message: 'Bag could not be created!' });
+    }); */
 };
 
 exports.editItem = (req, res) => {
@@ -15,72 +29,15 @@ exports.editItem = (req, res) => {
 exports.deleteItem = (req, res) => {
 
 };
-/* 
 
-const Bag = require('../models/bagModel');
-
-exports.getBags = (req, res) => {
-    Bag.findAllBags(req.userData.userID).then((result)=>{
-        if(result instanceof Error)
-            throw result;
-        else
-            console.log('Returned Result', result);
-            res.status(200).json({ data: result });
-    }).catch(e=>{
-        res.status(404).json({ message: e.message });
-    });
-};
-
-exports.createBag = (req, res) => {
-    let data = processBagOptionals(req.body);
-    Bag.createBag(data, req.userData.userID).then((result)=>{
-        res.status(201).json({ message: 'New Bag Created', data: result });
-    }).catch(e=>{
-        res.status(500).json({ message: 'Bag could not be created!' });
-    });
-};
-
-
-exports.editBag = (req, res) => {
-    let data = processBagOptionals(req.body);
-    Bag.editBag(req.params.id, req.userData.userID, data).then((result)=>{
-        if(result instanceof Error)
-            throw result;
-        else
-            res.status(200).json({ message: 'Bag has been Updated!', data: result});
-    }).catch(e=>{
-        res.status(500).json({ message: `${e.message} Bag could not be updated!` });
-    });
-};
-
-exports.deleteBag = (req, res) => {
-    Bag.deleteBag(req.params.id, req.userData.userID).then((result)=>{
-        if(result instanceof Error)
-            throw result;
-        else
-            res.status(200).json({ message: 'Bag has been Deleted!', data: result});
-    }).catch(e=>{
-        res.status(500).json({ message: `${e.message} Bag could not be deleted!` });
-    });
-};
-
-function processBagOptionals(data) {
-    if(!data.bvolume)
-        data.bvolume = null;
-    if(!data.bweight)
-        data.bweight = null;
+function processItemOptionals(data) {
+    if(!data.image)
+        data.image = null;
+    if(!data.ivolume)
+        data.ivolume = null;
+    if(!data.iweight)
+        data.iweight = null;
+    if(!data.priority)
+        data.priority = null;
     return data;
 };
-
-
-    // check if bag is empty first
-    // ask user if they want to move items to another bag first, change item FK bid to another bag
-    // if not tell them it will delete all the contents of the bag
-    /*
-        1. delete items with FK bid of the bag
-
-            THEN
-
-        2. delete the bag with PK bid
-    
-    */ */
--- models/itemModel.js
+++ models/itemModel.js
@@ -1,8 +1,22 @@
 const db = require('../pgConnect');
 
-exports.findAllItems = async (bid) => {
-
-
+exports.findAllItems = async (uid, bid) => {
+    try {
+        const UserBags = await db.sql`SELECT bid FROM bags WHERE uid = ${uid}`;
+        if(UserBags.length===0) {
+            return Error('This bag does not exist.');
+        } else if(UserBags.some(bag=>bag.bid!==bid)) { 
+            return Error('This is not your bag!');
+        } else {
+            const hasAnyItems = await db.sql`SELECT iid, iname, idesc FROM items WHERE bid = ${bid}`;
+            if(hasAnyBags.length===0)
+                return Error('You do not have any items in this bag.');
+            else
+                return hasAnyItems;
+        } 
+    } catch(error) {
+        console.log(error);
+    }
 };
 
 exports.createItem = async (data, bid) => {
@@ -19,3 +33,17 @@ exports.deleteItem = async (iid, uid) => {
 
 
 };
+
+/* try {
+    const BagExists = await db.sql`SELECT * FROM bags WHERE bid = ${bid}`;
+    if(BagExists.length===0){
+        return Error('This bag does not exist.');
+    } else if(BagExists[0].uid!==uid){
+        return Error('This is not your bag!');
+    } else {
+        const result = await db.sql`UPDATE bags SET bname=${data.bname}, bvolume=${data.bvolume}, bweight=${data.bweight} WHERE bid = ${bid} RETURNING *`;
+        return result[0];
+    }
+} catch(error) {
+    console.log(error);
+}    */
