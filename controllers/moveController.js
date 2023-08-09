73d0da6deb05f049561acad6c400aaa949a4cb15
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-08-09 12:03:46 -0400

    move controller added error catching

--- controllers/moveController.js
+++ controllers/moveController.js
@@ -1,11 +1,29 @@
 const Move = require('../models/moveModel');
 
 exports.moveAllItems = (req, res) => {
-
-
+    Move.moveAllItems(req.userData.userID, req.body).then((result)=>{
+        if(result instanceof Error){
+            throw result;
+        }   
+        else {
+            // TO DO
+        }
+    }).catch(e=>{
+        res.status(500).json({ message: `${e.message} Items could not be transfered!` });
+    });
 };
 
 exports.moveOneItem = (req, res) => {
-
-
+    Move.moveOneItem(req.userData.userID, req.nbid, req.params.id).then((result)=>{
+        if(result instanceof Error){
+            throw result;
+        }   
+        else {
+            // TO DO
+        }
+    }).catch(e=>{
+        res.status(500).json({ message: `${e.message} Item could not be transfered!` });
+    });
 };
+
+uid, nbid, iid
