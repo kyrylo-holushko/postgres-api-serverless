6c590b59865b594732f89cd187ea125edc301778
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-08-21 13:44:23 -0400

    fixed formatting

--- pgConnect.js
+++ pgConnect.js
@@ -5,11 +5,11 @@ dotenv.config();
 const { PGHOST, PGDATABASE, PGUSERNAME, PGPASSWORD } = process.env;
 
 const sql = postgres({
-  host                 : PGHOST,            // Postgres ip address[s] or domain name[s]
-  port                 : 5432,          // Postgres server port[s]
-  database             : PGDATABASE,            // Name of database to connect to
-  username             : PGUSERNAME,            // Username of database user
-  password             : PGPASSWORD            // Password of database user
+    host                 : PGHOST,            // Postgres ip address[s] or domain name[s]
+    port                 : 5432,          // Postgres server port[s]
+    database             : PGDATABASE,            // Name of database to connect to
+    username             : PGUSERNAME,            // Username of database user
+    password             : PGPASSWORD            // Password of database user
 });
 
 console.log('Connected to Postgres DB:',PGHOST);
--- server.js
+++ server.js
@@ -21,33 +21,33 @@ app.use(express.json({ limit: '1kb' }));
 /* MIDDLEWARE */
 
 const singUpLimiter = rateLimit({
-  max: 4, // 4 requests per hour
-  windowMs: 60 * (60000), //60 minutes limit
-  message: 'Too many accounts created from this IP, please try again after an hour',
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    max: 4, // 4 requests per hour
+    windowMs: 60 * (60000), //60 minutes limit
+    message: 'Too many accounts created from this IP, please try again after an hour',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 checkAuth = (req, res, next) => {
-  if(req.method==='OPTIONS')
-    return next();
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    if(!token) {
-      throw Error('Authentication Failed!');
+    if(req.method==='OPTIONS')
+        return next();
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if(!token) {
+        throw Error('Authentication Failed!');
+        }
+        const decodedToken = jwt.verify(token, JWTSECRET);
+        req.userData = {userID: decodedToken.uid};
+        next();
+    } catch(error){
+        return next(error);
     }
-    const decodedToken = jwt.verify(token, JWTSECRET);
-    req.userData = {userID: decodedToken.uid};
-    next();
-  } catch(error){
-    return next(error);
-  }
 };
 
 /* ROUTES */
 
 app.get('/', (req, res) => {
-  res.json({ message: 'root'});
+    res.json({ message: 'root'});
 });
 
 /* USER CRUD */
@@ -76,7 +76,7 @@ app.put('/api/move/item/:id', checkAuth, moveController.moveOneItem);
 
 
 app.all('*', (req, res) => {
-  res.status(404).json({ message: '404 Page Not Found' });
+    res.status(404).json({ message: '404 Page Not Found' });
 });
 
 app.listen(PORT, ()=>{console.log("API listening on: " + PORT)});
