52c5370e1cb907a76acae61707b7b98107144d43
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-10-30 13:41:12 -0400

    added helmet protection

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const jwt = require('jsonwebtoken');
 const cors = require("cors");
 const multer = require('multer');
+const helmet = require('helmet');
 
 /* Multipart Form Data */
 
@@ -22,7 +23,7 @@ dotenv.config();
 const { PORT, JWTSECRET } = process.env;
 
 app.use(cors());
-
+app.use(helmet());
 app.use(express.json({ limit: '1mb' }));
 
 /* MIDDLEWARE */
