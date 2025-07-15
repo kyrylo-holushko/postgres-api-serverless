da68e6a40c464ab351eb46cb17ca8cb6ac20b827
kyrylo-holushko <kyrylo_holushko@gmail.com>
2025-07-15 16:29:09 -0400

    Updated connection file for new db host

--- src/pgConnect.js
+++ src/pgConnect.js
@@ -1,16 +1,19 @@
 const postgres = require('postgres');
 const dotenv = require('dotenv');
 dotenv.config();
-const { PGHOST, PGDATABASE, PGUSERNAME, PGPASSWORD } = process.env;
+//const { PGHOST, PGDATABASE, PGUSERNAME, PGPASSWORD } = process.env;
+const connectionString = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
 
-const sql = postgres({
+/* const sql = postgres({
     host                 : PGHOST,            // Postgres ip address[s] or domain name[s]
     port                 : 5432,          // Postgres server port[s]
     database             : PGDATABASE,            // Name of database to connect to
     username             : PGUSERNAME,            // Username of database user
-    password             : PGPASSWORD            // Password of database user
-});
+    password             : PGPASSWORD,            // Password of database user
+    ssl                  : 'require'
+}); */
 
-console.log('Connected to Postgres DB:',PGHOST);
+console.log('Connected to Postgres DB');
 
 module.exports.sql = sql;
